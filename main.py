97162b99ec54a6a23df8d38341d59b1978a3c636
AlphaUnderscoreX <23343507+AlphaUnderscoreX@users.noreply.github.com>
2019-06-16 10:08:59 +0530

    Advancing master branch in parallel

--- main.py
+++ main.py
@@ -2,3 +2,9 @@
 
 if __name__ == '__main__':
 	print 'On the master'
+	print 'Advancing master branch'
+
+	has_branch_created()
+
+def has_branch_created():
+	print "Branch 'first' has been created"
